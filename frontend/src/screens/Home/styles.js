76fabd8e793ddcd1736fad26f693177b7ae08f50
João Marcos Magalhães Madureira <joaommmadureira@hotmail.com>
2021-02-28 23:07:20 -0300

    Continuing to style the homepage

--- frontend/src/screens/Home/styles.js
+++ frontend/src/screens/Home/styles.js
@@ -1,34 +1,63 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
 
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
+
+export const Header = styled.span`
+    display: flex;
+    justify-content: flex-end;
+    margin: 1.5vh 2.5vw 0 0;
+
+    font-size: 2rem;
 `;
 
 export const Center = styled.div`
+    width: 80%;
+    height: 100%;
 
+    margin-left: 10vw;
+
+    flex-direction: column;
+    display: flex;
+    align-items: center;
   `;
      
 export const Title = styled.h1`
+    margin-top: 10vh;
     color: var(--purple);
     font-weight: 700;
-    font-size: 3rem;
+    font-size: 5rem;
+    text-align: center;
 `;
 
 export const Text = styled.p`
-    font-size: 1.5rem;
+    width: 40rem;
+    font-size: 2rem;
     font-weight: 700;
+    text-align: center;
 `;
 
 export const StartButton = styled.button`
-    height: 3rem;
-    width: 20%;
+    height: 3.5rem;
+    width: 15vw;
+
+    font-size: 2rem;
 
     display: flex;
-    margin-top: 2rem;
+    margin-top: 3rem;
     justify-content: center;
     align-items: center;
 
     border-radius: 30px;
     background: var(--pink);
     color: var(--white);
+`;
+
+export const Image = styled.img`
+    height: 15rem;
+    margin-top: 4rem;
 `;
